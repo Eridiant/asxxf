0e518a6139ef4fee869c2a99f1032e8393b80697
Eridiant <eridiante@gmail.com>
2022-11-02 09:11:38 +0300

    add send sucsess

--- js/main.js
+++ js/main.js
@@ -5,13 +5,22 @@ document.addEventListener('DOMContentLoaded', () => {
     mailBtn.addEventListener('click', (e) => {
         e.preventDefault();
         modal.classList.add('active');
+        document.querySelector('.main-wrapper.main').classList.add('dn');
     })
 
     let modalClose = document.querySelector('.contact-modal-close');
 
-
     modalClose.addEventListener('click', (e) => {
         e.preventDefault();
         modal.classList.remove('active');
+        document.querySelector('.main-wrapper.main').classList.remove('dn');
+    })
+
+    let form = document.querySelector('.form-alt');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        modal.classList.remove('active');
+        document.querySelector('.main-wrapper.done').classList.remove('dn');
     })
 })
